932d6c86d2d17cdef209378f34a203b70a9bc264
DakotaOlha <ochaiuk2006@gmail.com>
2025-10-07 14:23:33 +0300

    feat(main): create tamplate feature fragments

---
+++ app/src/main/java/com/example/fopsmart/ui/report/ReportViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.fopsmart.ui.report
+
+import androidx.lifecycle.ViewModel
+
+class ReportViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/fopsmart/ui/stats/StatsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.fopsmart.ui.stats
+
+import androidx.lifecycle.ViewModel
+
+class StatsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}

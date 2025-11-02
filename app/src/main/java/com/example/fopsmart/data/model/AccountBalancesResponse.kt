98627fcedc91b727506eb7641548f26048d16ccd
DakotaOlha <ochaiuk2006@gmail.com>
2025-11-02 16:27:21 +0200

    fix(chat): fix bag with get balance

--- app/src/main/java/com/example/fopsmart/data/model/AccountBalancesResponse.kt
+++ app/src/main/java/com/example/fopsmart/data/model/AccountBalancesResponse.kt
@@ -3,6 +3,9 @@ package com.example.fopsmart.data.model
 import com.google.gson.annotations.SerializedName
 
 data class AccountBalancesResponse(
-    @SerializedName("balances")
-    val balances: List<AccountBalance> = emptyList()
+    @SerializedName("accounts")
+    val balances: List<AccountBalance> = emptyList(),
+
+    @SerializedName("total")
+    val total: List<TotalBalanceInfo> = emptyList()
 )
